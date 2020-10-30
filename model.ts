90477ac475f4661315b1c4d9be83f89bfb3b188d
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-10-30 20:17:59 +0800

    first commit

---
+++ model.ts
@@ -0,0 +1,16 @@
+
+export type Template = {
+  readonly targetDir: string
+  readonly targetFile: string
+  readonly templateFile: string
+}
+
+export type Command = {
+  readonly name: string
+  readonly args: ReadonlyArray<string>
+  readonly templates: ReadonlyArray<Template>
+}
+
+export type Config = {
+  readonly commands: ReadonlyArray<Command>
+}
