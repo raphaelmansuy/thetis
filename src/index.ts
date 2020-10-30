89aa5fbf5a6f4d83574fa3f818dca4890e77e41a
Raphael MANSUY <raphael.mansuy@caliatys.com>
2020-10-30 22:16:44 +0800

    improve cli

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-#!usr/bin/env node
+#!/bin/sh -
+':' /*-
+test1=$(nodejs --version 2>&1) && exec nodejs "$0" "$@"
+test2=$(node --version 2>&1) && exec node "$0" "$@"
+exec printf '%s\n' "$test1" "$test2" 1>&2
+*/
+
 
 import reader from "readline-sync"
 import { ensureDirSync } from "fs-extra"
